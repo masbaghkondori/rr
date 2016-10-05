02425d9f0945ac2764bf3e67ad0b941d868f0ee7
mohbes <mohbes@IMM.INTRA>
2016-10-06 00:32:28 +0200

    fourt version

--- Project-Assignment04-MasoomehBaghban/src/com/assignment_4/main/Main.java
+++ Project-Assignment04-MasoomehBaghban/src/com/assignment_4/main/Main.java
@@ -1,26 +1,35 @@
 package com.assignment_4.main;
 
+import com.assignment_4.subclasses.BankCustomer;
 import com.assignment_4.subclasses.PersonalAccount;
 import com.assignment_4.subclasses.SavingAccount;
 import com.assignment_4.superclasses.BankAccount;
 
 public class Main {
 	public static void main(String[] args){
-		BankAccount bankAccount = new BankAccount();
-		System.out.println(bankAccount.toString());
-		bankAccount.depositMoney(200.0);
+		String account="";
+		BankCustomer bc = new BankCustomer("Rafael",30);
+		bc.addAccount(new BankAccount());
+		bc.addAccount(new PersonalAccount());
+		bc.addAccount(new SavingAccount());
+		System.out.println(bc.toString());
 		
-		PersonalAccount pa = new PersonalAccount();
-		pa.depositMoney(70);
-		System.out.println(pa.toString());
-		pa.depositMoney(-1);
+		account=bc.getCustomerAccounts().get(0).getAccountNumber();
 		
-		SavingAccount sa = new SavingAccount();
-		sa.withdrawMoney(-20);
-		sa.withdrawMoney(20);
-		System.out.println(sa.toString());
+		account=bc.getCustomerAccounts().get(0).getAccountNumber();
+		System.out.println("Depositing 30.0 to the first account");
+		bc.depositToAccount(account,30.0);
+		System.out.println(bc.toString());
 		
-	}
-
+		System.out.println("withdrawing 200.0 to the first account");;
+		account=bc.getCustomerAccounts().get(1).getAccountNumber();
+		bc.withdrawMOneyFromAccount(account,200.0);
+		System.out.println(bc.toString());
+		
+		System.out.println("Depositing 200.0 to the first account");
+		account=bc.getCustomerAccounts().get(2).getAccountNumber();
+		bc.depositToAccount(account,200.0);
+System.out.println(bc.toString());		
+		
 }
 
