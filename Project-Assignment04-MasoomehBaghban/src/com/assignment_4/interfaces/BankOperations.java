b42c3d2fb9949473d7952493435a6f1fb11515b7
mohbes <mohbes@IMM.INTRA>
2016-10-05 14:23:29 +0200

    first version

---
+++ Project-Assignment04-MasoomehBaghban/src/com/assignment_4/interfaces/BankOperations.java
@@ -0,0 +1,18 @@
+package com.assignment_4.interfaces;
+
+
+
+
+public interface BankOperations {
+	public void withdrawMoney(double amount);
+	public void depositMoney(double amount);
+
+}
+
+
+/**
+public interface BankOperations {
+	public void withdrawMoney(double amount);
+	public void depositMoney(double amount);
+
+}*/
